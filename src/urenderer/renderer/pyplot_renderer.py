9cecd496efab34831ad9a77b1f8d84ee7a75ae64
f250397 <f250397@dac.unicamp.br>
2026-05-11 15:43:32 -0300

    Teste_Mudancas_Renderer

--- src/urenderer/renderer/pyplot_renderer.py
+++ src/urenderer/renderer/pyplot_renderer.py
@@ -75,7 +75,7 @@ class PyplotRenderer(Renderer):
         # Projete o triângulo, combinando a matriz de transformação do modelo,
         # view matriz (self._view_matrix) e a matriz de projeção (self._projection_matrix)
 
-        triangle_proj = triangle @ self._view_matrix @ self._projection_matrix
+        triangle_proj = model_transformation @ self._view_matrix @ self._projection_matrix
 
         #########################################################################
 
@@ -108,21 +108,11 @@ class PyplotRenderer(Renderer):
         for i in range(3):
             x = triangle[i][0]
             y = triangle[i][1]
-            z = triangle[i][2]
-            w = triangle[i][3]
-            if (w < 0):
-                w = -w
+            w = triangle[i][2]
+            y = triangle[i][3]
             clip = clip and (-w <= x and x <= w) and (-w <= y and y <= w) and (-w <= z and z <= w)
 
         # Normalize o triângulo, dividindo cada vértice pelo seu último valor v_w
-        for i in range(3):
-            if (triangle[i][3] != 0):
-                triangle[i] = triangle[i]/triangle[i][3]
-            else:
-                triangle[i] = triangle @ [0, 0, 0, 0]
-
-        if not clip:
-            return (not clip), triangle
 
         return (not clip), triangle
         #########################################################################
@@ -144,9 +134,12 @@ class PyplotRenderer(Renderer):
         # A primeira coordenada deve ser mapeada para [0, self.screen_width]
         # A segunda coordenada deve ser mapeada para [0, self.screen_height]
         for i in range(3):
-            triangle[i][0] = ((triangle[i][0] + 1)/2) * self.screen_width
-            triangle[i][1] = ((triangle[i][1] + 1)/2) * self.screen_height
+            triangle[i][0] = (triangle[i][0] + 1)/2
+            triangle[i][1] = (triangle[i][1] + 1)/2
             triangle[i][2] = (triangle[i][2] + 1)/2
+        
+        triangle[0] = triangle[0] * self.screen_width
+        triangle[1] = triangle[1] * self.screen_height
 
         #########################################################################
 
