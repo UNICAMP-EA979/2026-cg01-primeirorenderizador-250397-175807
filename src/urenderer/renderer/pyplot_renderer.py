da4dd4da8eb81f6675452ee99a6ac131810818c5
f250397 <f250397@dac.unicamp.br>
2026-05-11 17:05:53 -0300

    Teste_Mudancas_Renderer

--- src/urenderer/renderer/pyplot_renderer.py
+++ src/urenderer/renderer/pyplot_renderer.py
@@ -75,12 +75,12 @@ class PyplotRenderer(Renderer):
         # Projete o triângulo, combinando a matriz de transformação do modelo,
         # view matriz (self._view_matrix) e a matriz de projeção (self._projection_matrix)
 
-        triangle_proj_1 =  self._view_matrix @ self._projection_matrix @ triangle @ model_transformation
-        triangle_proj_2 =  self._view_matrix @ self._projection_matrix @ model_transformation @ triangle
-        triangle_proj_3 =  self._view_matrix @ triangle @ model_transformation @ self._projection_matrix
-        triangle_proj_4 =  self._view_matrix @ triangle @ self._projection_matrix @ model_transformation
-        triangle_proj_5 =  self._view_matrix @ model_transformation @ triangle @ self._projection_matrix
-        triangle_proj_6 =  self._view_matrix @ model_transformation @ self._projection_matrix @ triangle
+        triangle_proj_1 =  self._projection_matrix @ self._view_matrix @ model_transformation @ triangle
+        triangle_proj_2 =  self._projection_matrix @ self._view_matrix @ triangle @ model_transformation
+        triangle_proj_3 =  self._projection_matrix @ triangle @ model_transformation @ self._view_matrix
+        triangle_proj_4 =  self._projection_matrix @ triangle @ self._view_matrix @ model_transformation
+        triangle_proj_5 =  self._projection_matrix @ model_transformation @ triangle @ self._view_matrix
+        triangle_proj_6 =  self._projection_matrix @ model_transformation @ self._view_matrix @ triangle
 
         triangle_proj = triangle_proj_1
         triangle_proj[0][1] = triangle_proj_2[0][1]
@@ -149,10 +149,9 @@ class PyplotRenderer(Renderer):
         # Mapeie o triângulo que está no intervalo [-1, 1]
         # A primeira coordenada deve ser mapeada para [0, self.screen_width]
         # A segunda coordenada deve ser mapeada para [0, self.screen_height]
-        for i in range(3):
-            triangle[i] = (triangle[i] + 1)/2
-        
-        triangle[0] *= self.screen_width
+        triangle[0] = ((triangle[0] + 1)/2) * self.screen_width
+        triangle[1] = ((triangle[1] + 1)/2) * self.screen_height
+        triangle[2] = ((triangle[2] + 1)/2)
 
         #########################################################################
 
