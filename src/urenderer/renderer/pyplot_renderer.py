5a8c0b6f54d4508faedb73a42878acd1c99c6f87
f250397 <f250397@dac.unicamp.br>
2026-05-11 21:03:27 -0300

    Teste_Mudancas_Renderer

--- src/urenderer/renderer/pyplot_renderer.py
+++ src/urenderer/renderer/pyplot_renderer.py
@@ -75,7 +75,7 @@ class PyplotRenderer(Renderer):
         # Projete o triângulo, combinando a matriz de transformação do modelo,
         # view matriz (self._view_matrix) e a matriz de projeção (self._projection_matrix)
 
-        triangle_proj =  triangle @ (self._projection_matrix @ self._view_matrix @ model_transformation)
+        triangle_proj =  triangle @ self._projection_matrix @ self._view_matrix @ model_transformation
 
 
         #########################################################################
@@ -137,7 +137,7 @@ class PyplotRenderer(Renderer):
         # Mapeie o triângulo que está no intervalo [-1, 1]
         # A primeira coordenada deve ser mapeada para [0, self.screen_width]
         # A segunda coordenada deve ser mapeada para [0, self.screen_height]
-        for i in range(4):
+        for i in range(3):
             triangle[0][i] = ((triangle[0][i] + 1)/2) * self.screen_width
             triangle[1][i] = ((triangle[1][i] + 1)/2) * self.screen_height
             triangle[2][i] = (triangle[2][i] + 1)/2
